1b202662b85d7319b79985cbfca6b724d2166b55
JayNirmal <nirmal.j@husky.neu.edu>
2018-03-29 12:28:26 -0400

    angular lecture

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import{Hero} from './hero';
+
+export const HEROES:Hero[] =[
+    {id:11,name:'Mr.Nice',age:12},
+    {id:12,name:'Jay',age:23},
+    {id:13,name:'AJ',age:21},
+    {id:14,name:'Ashish',age:25},
+    {id:15,name:'Ashok',age:null},
+    {id:16,name:'Vishal',age:22},
+    {id:17,name:'Raj',age:22},
+    {id:18,name:'Sameer',age:22},
+    {id:19,name:'Naman',age:22},
+    {id:20,name:'Gaurav',age:22}   
+];
